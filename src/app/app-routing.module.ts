23067392005075035f164f8d6605d332f272f4fb
unknown <mujtabakhalid1999@gmail.com>
2023-04-20 03:15:20 +0500

    navbar added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("../app/components/landing-page/landing-page.module").then(m => m.LandingPageModule)
+  },
+  { path: "**", redirectTo: "" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
