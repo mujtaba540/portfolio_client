064f9321478ef9ebb6f042bfd22d54827f5dfc9e
unknown <mujtabakhalid1999@gmail.com>
2023-06-26 22:10:17 +0500

    intro section added

--- src/app/components/landing-page/landing-page.module.ts
+++ src/app/components/landing-page/landing-page.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
+import { IntroSectionComponent } from './intro-section/intro-section.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
 
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    IntroSectionComponent,
+    SkillsSectionComponent,
   ],
   imports: [
     CommonModule,
