faec7e33f2d8dfc0e83827f2ed23c7f15336a011
unknown <mujtabakhalid1999@gmail.com>
2023-09-01 01:10:36 +0500

    new changes

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,11 +6,5 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  @ViewChild('contact') public structure !: ElementRef;
-  
-  naviagteToForm(){
-    console.log(this.structure);
-    this.structure.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'start' });
 
-  }
 }
