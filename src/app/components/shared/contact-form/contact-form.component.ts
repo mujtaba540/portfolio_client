e62ffca36a4b8a1684bb33c8c934eabb24263da4
unknown <mujtabakhalid1999@gmail.com>
2023-09-02 14:04:40 +0500

    new changes

--- src/app/components/shared/contact-form/contact-form.component.ts
+++ src/app/components/shared/contact-form/contact-form.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit{
+  emailForm!:FormGroup;
+  isSubmitting!:boolean;
+  
+  ngOnInit(): void {
+    this.emailForm=new FormGroup({
+      name:new FormControl(null,Validators.required),
+      email:new FormControl(null,[Validators.required,Validators.email]),
+      message:new FormControl(null,Validators.required)
+    })
+  }
+
+  sendEmail(){
+    console.log(this.emailForm);
+  }
 
 }
--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { WorkDetailsCardComponent } from './work-details-card/work-details-card.component';
 import { FooterComponent } from './footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -17,7 +18,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   exports:[
     ContactFormComponent,
